a1ace53b9378cc207b17824d9879bb6ee66bac57
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2023-07-27 01:43:37 -0400

    here's your fucking pr
    a code review is no reason for homophobia/transphobia

--- src/main/java/org/benefit/mixin/BookScreenMixin.java
+++ src/main/java/org/benefit/mixin/BookScreenMixin.java
@@ -1,135 +1,102 @@
 package org.benefit.mixin;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.*;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.network.*;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.gui.widget.TextWidget;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.c2s.play.CloseHandledScreenC2SPacket;
-import net.minecraft.screen.ScreenHandler;
 import net.minecraft.text.Text;
-import org.benefit.Client;
+import net.minecraft.util.Formatting;
 import org.benefit.Variables;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import static org.benefit.Client.mc;
 
 
 @Mixin(BookScreen.class)
 public abstract class BookScreenMixin extends Screen {
-    @Shadow
-    public abstract boolean keyPressed(int keyCode, int scanCode, int modifiers);
     protected BookScreenMixin(Text title) {
         super(title);
     }
-//define variables
-    private static final MinecraftClient mc = MinecraftClient.getInstance();
-    private static final ClientPlayerEntity mcp = mc.player;
 
-//the main method
     @Inject(at = @At("TAIL"), method = "init")
     public void init(CallbackInfo ci) {
-    //check if screen is a lecturn or book
-        if (client.currentScreen instanceof LecternScreen || client.currentScreen instanceof BookScreen || client.currentScreen instanceof BookEditScreen) {
         //simplify expressions
-            Boolean sendUIPackets = Variables.sendUIPackets;
-            ScreenHandler CurrentScreenHandler = mcp.currentScreenHandler;
-            Screen CurrentScreen = mc.currentScreen;
-            ClientPlayNetworkHandler mcNetworkHandler = mc.getNetworkHandler();
+        String bGray = Formatting.BOLD.toString() + Formatting.GRAY;
+        String bGreen = Formatting.BOLD.toString() + Formatting.GREEN;
 
-        //define variables
-            int syncID = mcp.currentScreenHandler.syncId;
-            CloseHandledScreenC2SPacket CloseHandledScreen = new CloseHandledScreenC2SPacket(syncID);
+        //display sync id
+        Text syncId = Text.of("Sync Id: " + mc.player.currentScreenHandler.syncId);
+        addDrawableChild(new TextWidget(4, 5, textRenderer.getWidth(syncId) + 4, 20, syncId, textRenderer));
 
-        //define color codes
-            String bold = "§l";
-            String lGray = "§7";
-            String green = "§a";
-
-        //combine bold and lightgray
-            String bGray = bold + lGray;
-
-        //combine bold and green
-            String bGreen = bold + green;
-
-        //define button names
-            Text softClose = Text.of("Soft Close");
-            Text deSync = Text.of("De-sync");
-            Text saveUI = Text.of("Save UI");
-            Text leaveNSend = Text.of("Leave & send packets");
-
+        //display revision
+        Text revision = Text.of("Revision: " + mc.player.currentScreenHandler.getRevision());
+        addDrawableChild(new TextWidget(4, 25, textRenderer.getWidth(revision) + 4, 20, revision, textRenderer));
 
         //add in send packets button
-            addDrawableChild(ButtonWidget.builder(Text.of("Send Packets: " + sendUIPackets), button -> {
-                Variables.sendUIPackets = !Variables.sendUIPackets;
+        addDrawableChild(ButtonWidget.builder(Text.of("Send Packets: " + Variables.sendUIPackets), button -> {
+            Variables.sendUIPackets = !Variables.sendUIPackets;
 
             //setting the text on the button to true or false when it is active
-                button.setMessage(Text.of("Send Packets: " + Variables.sendUIPackets));
-
-            }).dimensions(4, 65, 120, 20).build());
+            button.setMessage(Text.of("Send Packets: " + Variables.sendUIPackets));
+        }).dimensions(4, 105, 120, 20).build());
 
         //add in delay packets button
-            addDrawableChild(ButtonWidget.builder(Text.of("Delay packets: " + Variables.delayUIPackets), (button) -> {
-                Variables.delayUIPackets = !Variables.delayUIPackets;
+        addDrawableChild(ButtonWidget.builder(Text.of("Delay packets: " + Variables.delayUIPackets), (button) -> {
+            Variables.delayUIPackets = !Variables.delayUIPackets;
 
             //setting the text on the button to true or false when it is active
-                button.setMessage(Text.of("Delay packets: " + Variables.delayUIPackets));
+            button.setMessage(Text.of("Delay packets: " + Variables.delayUIPackets));
 
             //condition to see if any delayed packets was delayed, then send them
-                if (!Variables.delayUIPackets && !Variables.delayedPackets.isEmpty()) {
-                    for (Packet<?> packet : Variables.delayedPackets) {
-                        mc.getNetworkHandler().sendPacket(packet);
-                    }
+            if (!Variables.delayUIPackets && !Variables.delayedPackets.isEmpty()) {
+                for (Packet<?> packet : Variables.delayedPackets) {
+                    mc.getNetworkHandler().sendPacket(packet);
+                }
 
                 //add in message to say how many delayed packets were sent
-                    int DelayedPacketsCount = Variables.delayedPackets.size();
-                    mc.player.sendMessage(Text.of(bGray + "Successfully sent " + bGreen + DelayedPacketsCount + lGray + " delayed packets."));
-                    Variables.delayedPackets.clear();
-                }
-            }).width(120).position(4, 35).build());
+                int DelayedPacketsCount = Variables.delayedPackets.size();
+                mc.player.sendMessage(Text.of(bGray + "Successfully sent " + bGreen + DelayedPacketsCount + Formatting.GRAY + " delayed packets."));
+                Variables.delayedPackets.clear();
+            }
+        }).width(120).position(4, 75).build());
 
         //add in softclose button
-            addDrawableChild(ButtonWidget.builder(softClose, (button) -> mc.setScreen(null)).width(80).position(4, 5).build());
+        addDrawableChild(ButtonWidget.builder(Text.of("Soft Close"), (button) -> mc.setScreen(null)).width(80).position(4, 45).build());
 
         //add in desync button
-            addDrawableChild(ButtonWidget.builder(deSync, (button) -> mcNetworkHandler.sendPacket(CloseHandledScreen)).width(80).position(4, 155).build());
+        addDrawableChild(ButtonWidget.builder(Text.of("De-sync"), (button) -> {
+            int syncID = mc.player.currentScreenHandler.syncId;
+            mc.getNetworkHandler().sendPacket(new CloseHandledScreenC2SPacket(syncID));
+        }).width(80).position(4, 195).build());
 
         //add in save ui button
-            addDrawableChild(ButtonWidget.builder(saveUI, (button) -> {
-
+        addDrawableChild(ButtonWidget.builder(Text.of("Save UI"), (button) -> {
             //define variables
-                Variables.storedScreen = CurrentScreen;
-                Variables.storedScreenHandler = CurrentScreenHandler;
-
-            }).width(80).position(4, 95).build());
+            Variables.storedScreen = mc.currentScreen;
+            Variables.storedScreenHandler = mc.player.currentScreenHandler;
+        }).width(80).position(4, 135).build());
 
         //add in leave n send packets button
-            addDrawableChild(ButtonWidget.builder(leaveNSend, (button) -> {
+        addDrawableChild(ButtonWidget.builder(Text.of("Leave & send packets"), (button) -> {
 
-                if (!Variables.delayedPackets.isEmpty()) {
-                    Variables.delayUIPackets = false;
+            if (!Variables.delayedPackets.isEmpty()) {
+                Variables.delayUIPackets = false;
 
-                    for (Packet<?> packet : Variables.delayedPackets) {
-                        mc.getNetworkHandler().sendPacket(packet);
-                    }
+                for (Packet<?> packet : Variables.delayedPackets) {
+                    mc.getNetworkHandler().sendPacket(packet);
+                }
                 //add in message to say how many delayed packets were sent
-                    int DelayedPacketsAmount = Variables.delayedPackets.size();
+                int DelayedPacketsAmount = Variables.delayedPackets.size();
 
                 //disconnect player
-                    mc.getNetworkHandler().getConnection().disconnect(Text.of(bGray + "Disconnected, " + bGreen + DelayedPacketsAmount + bGray + " packets successfully sent."));
-                    Variables.delayedPackets.clear();
-                }
-            }).width(140).position(4, 125).build());
-        }
+                mc.getNetworkHandler().getConnection().disconnect(Text.of(bGray + "Disconnected, " + bGreen + DelayedPacketsAmount + bGray + " packets successfully sent."));
+                Variables.delayedPackets.clear();
+            }
+        }).width(140).position(4, 165).build());
     }
-    //make sinkid and revision visible
-        @Inject(at = @At("TAIL"), method = "render")
-        public void render (MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci){
-            Client.renderHandledScreen(mc, textRenderer, matrices);
-        }
 }
--- src/main/java/org/benefit/mixin/HandledScreenMixin.java
+++ src/main/java/org/benefit/mixin/HandledScreenMixin.java
@@ -1,117 +1,89 @@
 package org.benefit.mixin;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
-import net.minecraft.client.gui.screen.ingame.SignEditScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.gui.widget.TextWidget;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.c2s.play.CloseHandledScreenC2SPacket;
-import net.minecraft.screen.ScreenHandler;
 import net.minecraft.text.Text;
-import org.benefit.Client;
+import net.minecraft.util.Formatting;
 import org.benefit.Variables;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import static org.benefit.Client.mc;
+
 
 @Mixin(HandledScreen.class)
 public abstract class HandledScreenMixin extends Screen {
-    @Shadow
-    public abstract boolean keyPressed(int keyCode, int scanCode, int modifiers);
-
-    @Shadow protected abstract void init();
-
     protected HandledScreenMixin(Text title) {
         super(title);
     }
-//define variables
-    private static final MinecraftClient mc = MinecraftClient.getInstance();
-    private static final ClientPlayerEntity mcp = mc.player;
 
-//the main method
+    //the main method
     @Inject(at = @At("TAIL"), method = "init")
     public void init(CallbackInfo ci) {
+        //simplify expressions
+        String bGray = Formatting.BOLD.toString() + Formatting.GRAY;
+        String bGreen = Formatting.BOLD.toString() + Formatting.GREEN;
 
-    //simplify expressions
-        boolean sendUIPackets = Variables.sendUIPackets;
-        ScreenHandler currentScreenHandler = mcp.currentScreenHandler;
-        Screen currentScreen = mc.currentScreen;
-        ClientPlayNetworkHandler mcNetworkHandler = mc.getNetworkHandler();
+        //display sync id
+        Text syncId = Text.of("Sync Id: " + mc.player.currentScreenHandler.syncId);
+        addDrawableChild(new TextWidget(4, 5, textRenderer.getWidth(syncId) + 4, 20, syncId, textRenderer));
 
-    //define variables
-        int syncID = mcp.currentScreenHandler.syncId;
-        CloseHandledScreenC2SPacket closeHandledScreen = new CloseHandledScreenC2SPacket(syncID);
+        //display revision
+        Text revision = Text.of("Revision: " + mc.player.currentScreenHandler.getRevision());
+        addDrawableChild(new TextWidget(4, 25, textRenderer.getWidth(revision) + 4, 20, revision, textRenderer));
 
-    //define color codes
-        String bold = "§l";
-        String lGray = "§7";
-        String green = "§a";
+        //add in send packets button
+        addDrawableChild(ButtonWidget.builder(Text.of("Send Packets: " + Variables.sendUIPackets), button -> {
+            Variables.sendUIPackets = !Variables.sendUIPackets;
 
-    //combine bold and lightgray
-        String bGray = bold + lGray;
+            //setting the text on the button to true or false when it is active
+            button.setMessage(Text.of("Send Packets: " + Variables.sendUIPackets));
+        }).dimensions(4, 105, 120, 20).build());
 
-    //combine bold and green
-        String bGreen = bold + green;
-
-    //define button names
-        Text softClose = Text.of("Soft Close");
-        Text deSync = Text.of("De-sync");
-        Text saveUI = Text.of("Save UI");
-        Text leaveNSend = Text.of("Leave & send packets");
-
-
-    //add in send packets button
-        addDrawableChild(ButtonWidget.builder(Text.of("Send Packets: " + sendUIPackets), button -> {
-                    Variables.sendUIPackets = !Variables.sendUIPackets;
-
-               //setting the text on the button to true or false when it is active
-                    button.setMessage(Text.of("Send Packets: " + Variables.sendUIPackets));
-
-                }).dimensions(4, 95, 120, 20).build());
-
-    //add in delay packets button
+        //add in delay packets button
         addDrawableChild(ButtonWidget.builder(Text.of("Delay packets: " + Variables.delayUIPackets), (button) -> {
             Variables.delayUIPackets = !Variables.delayUIPackets;
 
-        //setting the text on the button to true or false when it is active
+            //setting the text on the button to true or false when it is active
             button.setMessage(Text.of("Delay packets: " + Variables.delayUIPackets));
 
-        //condition to see if any delayed packets was delayed, then send them
+            //condition to see if any delayed packets was delayed, then send them
             if (!Variables.delayUIPackets && !Variables.delayedPackets.isEmpty()) {
                 for (Packet<?> packet : Variables.delayedPackets) {
                     mc.getNetworkHandler().sendPacket(packet);
                 }
 
-            //add in message to say how many delayed packets were sent
+                //add in message to say how many delayed packets were sent
                 int DelayedPacketsCount = Variables.delayedPackets.size();
-                mc.player.sendMessage(Text.of(bGray + "Successfully sent " + bGreen + DelayedPacketsCount + lGray + " delayed packets."));
+                mc.player.sendMessage(Text.of(bGray + "Successfully sent " + bGreen + DelayedPacketsCount + Formatting.GRAY + " delayed packets."));
                 Variables.delayedPackets.clear();
             }
-        }).width(120).position(4, 125).build());
+        }).width(120).position(4, 75).build());
 
-    //add in softclose button
-        addDrawableChild(ButtonWidget.builder(softClose, (button) -> mc.setScreen(null)).width(80).position(4, 65).build());
+        //add in softclose button
+        addDrawableChild(ButtonWidget.builder(Text.of("Soft Close"), (button) -> mc.setScreen(null)).width(80).position(4, 45).build());
 
-    //add in desync button
-        addDrawableChild(ButtonWidget.builder(deSync, (button) -> mcNetworkHandler.sendPacket(closeHandledScreen)).width(80).position(4, 215).build());
+        //add in desync button
+        addDrawableChild(ButtonWidget.builder(Text.of("De-sync"), (button) -> {
+            int syncID = mc.player.currentScreenHandler.syncId;
+            mc.getNetworkHandler().sendPacket(new CloseHandledScreenC2SPacket(syncID));
+        }).width(80).position(4, 195).build());
 
-    //add in save ui button
-        addDrawableChild(ButtonWidget.builder(saveUI, (button) -> {
-        //define variables
-            Variables.storedScreen = currentScreen;
-            Variables.storedScreenHandler = currentScreenHandler;
-            client.setScreen(null);
-        }).width(80).position(4, 155).build());
+        //add in save ui button
+        addDrawableChild(ButtonWidget.builder(Text.of("Save UI"), (button) -> {
+            //define variables
+            Variables.storedScreen = mc.currentScreen;
+            Variables.storedScreenHandler = mc.player.currentScreenHandler;
+        }).width(80).position(4, 135).build());
 
-    //add in leave n send packets button
-        addDrawableChild(ButtonWidget.builder(leaveNSend, (button) -> {
+        //add in leave n send packets button
+        addDrawableChild(ButtonWidget.builder(Text.of("Leave & send packets"), (button) -> {
 
             if (!Variables.delayedPackets.isEmpty()) {
                 Variables.delayUIPackets = false;
@@ -119,24 +91,13 @@ public abstract class HandledScreenMixin extends Screen {
                 for (Packet<?> packet : Variables.delayedPackets) {
                     mc.getNetworkHandler().sendPacket(packet);
                 }
-            //add in message to say how many delayed packets were sent
+                //add in message to say how many delayed packets were sent
                 int DelayedPacketsAmount = Variables.delayedPackets.size();
-                
-            //disconnect player
+
+                //disconnect player
                 mc.getNetworkHandler().getConnection().disconnect(Text.of(bGray + "Disconnected, " + bGreen + DelayedPacketsAmount + bGray + " packets successfully sent."));
                 Variables.delayedPackets.clear();
             }
-        }).width(140).position(4, 185).build());
-        if ((Object) this instanceof SignEditScreen) {
-            //set the screen to null with softclose button
-            addDrawableChild(ButtonWidget.builder(Text.of("Soft Close"), (button) -> MinecraftClient.getInstance().setScreen(null)).width(80).position(5, 5).build());
-        }
-    }
-
-
-//make sinkid and revision visible
-    @Inject(at = @At("TAIL"), method = "render")
-    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        Client.renderHandledScreen(mc, textRenderer, matrices);
+        }).width(140).position(4, 165).build());
     }
 }
